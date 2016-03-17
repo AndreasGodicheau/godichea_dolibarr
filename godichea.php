73fbd5b340c89b24693fa08010571bdcc0594043
Andreas Godicheau <godichea@pc-b101-03.iut-valence.fr>
2016-03-17 11:09:10 +0100

    correction page godichea.php

--- godichea.php
+++ godichea.php
@@ -5,12 +5,12 @@
 	dol_include_once('societe/class/societe.class.php');
 	dol_include_once('core/lib/company.lib.php');
 	
-	$fk_soc = GETPOST('id')
+	$fk_soc = GETPOST('id');
 
 	$object = new Societe($db);
 	$object->fetch($fk_soc);
 	
-	$action = GETPOST('action')
+	$action = GETPOST('action');
 	
 	switch ($action){
 		case 'value':
@@ -22,19 +22,18 @@
 	
 	function _card(&object){
 
-	global $conf, $db, $user, $langs;
-	
-	
-	
-	$head = societe_prepare_head($object);
-	
-	llxHeader();
-	
-	dol_fiche_head($head, 'godichea', $langs->trans("ThirdParty"), 0, company);
-	
-	dol_fiche_end();
-	
-	llxFooter();
-}
+		global $conf, $db, $user, $langs;
+		
+		
+		$head = societe_prepare_head($object);
+		
+		llxHeader();
+		
+		dol_fiche_head($head, 'godichea', $langs->trans("ThirdParty"), 0, company);
+		
+		dol_fiche_end();
+		
+		llxFooter();
+	}
 
 ?>
