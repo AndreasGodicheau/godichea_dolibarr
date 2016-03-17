792da7f7961cd4e200e558ab7f0518ad6bc5b5ac
Andreas Godicheau <godichea@pc-b123-06.iut-valence.fr>
2016-03-17 09:41:44 +0100

    ajout onglet en config admin

--- lib/godichea.lib.php
+++ lib/godichea.lib.php
@@ -40,6 +40,10 @@ function godicheaAdminPrepareHead()
     $head[$h][1] = $langs->trans("About");
     $head[$h][2] = 'about';
     $h++;
+	$head[$h][0] = dol_buildpath("/godichea/admin/godichea_gestionpc.php", 1);
+    $head[$h][1] = $langs->trans("GestionPC");
+    $head[$h][2] = 'gestionpc';
+    $h++;
 
     // Show more tabs from modules
     // Entries must be declared in modules descriptor with line
